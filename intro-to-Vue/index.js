85c026747b9d1f84ccc171d34b4ddd239eb166c2
kelvin9314 <kelvin9314@gmail.com>
2020-04-17 00:41:45 +0800

    list rendering & event handling

--- intro-to-Vue/index.js
+++ intro-to-Vue/index.js
@@ -5,6 +5,28 @@ const app = new Vue({
     image: './assets/vmSocks-green.jpg',
     inVentory: 100,
     inStock: true,
-    onSale: true
-  }
+    onSale: true,
+    details: ["80% cotton","20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        id: 2234,
+        color: "green",
+        image: "./assets/vmSocks-green.jpg"
+      },
+      {
+        id: 2235,
+        color: "blue",
+        image: "./assets/vmSocks-blue.png"
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart +=1
+    },
+    updateProduct(newImage) {
+      this.image = newImage
+    }
+  },
 })
