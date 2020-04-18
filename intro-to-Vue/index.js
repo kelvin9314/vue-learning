99aa7a1e000c4105a6247679c68d652a890a9901
kelvin9314 <kelvin9314@gmail.com>
2020-04-19 02:01:58 +0800

    Computed Properties

--- intro-to-Vue/index.js
+++ intro-to-Vue/index.js
@@ -1,22 +1,25 @@
 const app = new Vue({
   el: '#app',
   data: {
+    brand: 'XYZ',
     product: 'Socket',
-    image: './assets/vmSocks-green.jpg',
+    // image: './assets/vmSocks-green.jpg',
+    selectedVariant: 0,
     inVentory: 0,
-    inStock: false,
     onSale: true,
     details: ["80% cotton","20% polyester", "Gender-neutral"],
     variants: [
       {
         id: 2234,
         color: "green",
-        image: "./assets/vmSocks-green.jpg"
+        image: "./assets/vmSocks-green.jpg",
+        quantity: 12
       },
       {
         id: 2235,
         color: "blue",
-        image: "./assets/vmSocks-blue.png"
+        image: "./assets/vmSocks-blue.png",
+        quantity: 0
       }
     ],
     cart: 0
@@ -25,8 +28,22 @@ const app = new Vue({
     addToCart() {
       this.cart +=1
     },
-    updateProduct(newImage) {
-      this.image = newImage
+    updateProduct(index) {
+      this.selectedVariant = index
+    }
+  },
+  computed: {
+    title() {
+      return `${this.brand}: ${this.product}`
+    },
+    image() {
+      return this.variants[this.selectedVariant].image
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity
+    },
+    sale() {
+      return this.onSale ? `${this.brand} ${this.product} are on sale` : `${this.brand} ${this.product} are not on sale` 
     }
   },
 })
