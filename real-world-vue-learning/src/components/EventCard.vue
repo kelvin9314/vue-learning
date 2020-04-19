ef8f5cd30d1ae35ebf2bf292ccffaf3d683d52aa
kelvin9314 <kelvin9314@gmail.com>
2020-04-19 16:27:06 +0800

    eeslint and editor setting

---
+++ real-world-vue-learning/src/components/EventCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 v-if="condition" v-on:event="handle"></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "hello world"
+    }
+  },
+}
+</script>
+
+<style></stcopy relyle>
