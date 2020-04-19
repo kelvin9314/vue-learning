a8e7bac8522e8197279e18fb457799e437972c90
kelvin9314 <kelvin9314@gmail.com>
2020-04-19 22:53:39 +0800

    State & Getters

--- real-world-vue-learning/src/store/index.js
+++ real-world-vue-learning/src/store/index.js
@@ -4,8 +4,37 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Kelvin Mok' },
+    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false },
+    ],
+    events: [
+      { id: 1, title: '...', organizer: '...' },
+      { id: 2, title: '...', organizer: '...' },
+      { id: 3, title: '...', organizer: '...' },
+      { id: 4, title: '...', organizer: '...' },
+    ],
+  },
   mutations: {},
   actions: {},
   modules: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    activeTodosCount: state => {
+      return state.todos.filter(todo => !todo.done).length
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    },
+  },
 })
--- real-world-vue-learning/src/views/EventCreate.vue
+++ real-world-vue-learning/src/views/EventCreate.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-    <h1>Create Event here</h1>
+    <h1>Create Event, {{ user.name }}</h1>
+    <p>This event was created bt {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
+    <p>{{ getEventById(1) }}</p>
   </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapState(['user', 'categories']),
+    ...mapGetters(['catLength', 'getEventById']),
+  },
+}
+</script>
