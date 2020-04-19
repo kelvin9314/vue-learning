c7575353decc8a6e530fe7ffe29c48f3031e040d
kelvin9314 <kelvin9314@gmail.com>
2020-04-19 20:50:04 +0800

    Global Components

--- real-world-vue-learning/src/components/EventCard.vue
+++ real-world-vue-learning/src/components/EventCard.vue
@@ -3,6 +3,7 @@
     <div class="event-card -shadow">
       <span class="eyebrow">At {{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
+      <BaseIcon name="users" />
       <span>{{ event.attendess.length }} attending</span>
     </div>
   </router-link>
